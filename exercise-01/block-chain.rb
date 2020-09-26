c17203f90b2fd800f7f9b69650c51edd6e80395f
Adriano Costa <adrianokosta001@gmail.com>
2020-09-26 14:42:52 -0300

    Add method to get last block hash

--- exercise-01/block-chain.rb
+++ exercise-01/block-chain.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 require_relative 'block.rb'
 
 class BlockChain
-  BLOCKS_FILES_NAMES = "blocks/block*.txt"
+  BLOCKS_FILES_NAMES = "blocks/block_*.txt"
 
   def create_block(sender:, recipient:)
     Block.new(sender: sender, recipient: recipient)
@@ -21,9 +21,35 @@ class BlockChain
   def persist block_message
     create_directory
 
+    block = mount_block block_message
+
     file = File.open("blocks/block_#{persisted_blocks_length + 1}.txt", "w")
 
-    file.puts(block_message)
+    file.puts(block)
+
+    file.close
+  end
+
+  def get_previous_block_hash
+    last_block_path = "blocks/block_#{persisted_blocks_length}.txt"
+
+    return mount_previous_hash 'Vazio' unless File.exist? last_block_path
+
+    file = File.open(last_block_path)
+    content = file.read
+    file.close
+
+    previous_hash = content.scan(/Hash: (.*)\n/).first.first
+
+    mount_previous_hash previous_hash
+  end
+
+  def mount_block block_message
+    block_message + get_previous_block_hash
+  end
+
+  def mount_previous_hash hash
+    "Hash Anterior: #{hash}"
   end
 
   def create_directory
